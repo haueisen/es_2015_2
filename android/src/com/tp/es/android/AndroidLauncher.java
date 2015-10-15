42e0e0235674080dc4a4b4c595ec67a787618877
Flávio Haueisen <flavio.haueisen@gmail.com>
2015-10-15 09:25:55 -0300

    Criação do Projeto

---
+++ android/src/com/tp/es/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tp.es.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tp.es.GameClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameClass(), config);
+	}
+}
---
+++ desktop/src/com/tp/es/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tp.es.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tp.es.GameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
+}
